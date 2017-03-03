83676f1559fd27b90855a2095ec455036a89c320
Frank Hunleth <fhunleth@troodon-software.com>
2017-03-03 11:57:59 -0500

    Add moduledoc

--- lib/elixir_ale.ex
+++ lib/elixir_ale.ex
@@ -1,2 +1,7 @@
 defmodule ElixirALE do
+  @moduledoc """
+  `elixir_ale` provides high level abstractions for interfacing to GPIOs, I2C
+  buses and SPI peripherals on Linux platforms. Internally, it uses the Linux
+  sysclass interface so that it does not require platform-dependent code.
+  """
 end
